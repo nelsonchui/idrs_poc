ce1607a0eb7532f7f3399937b2fd63c53ebd7b6e
nelsonchui <47563105+nelsonchui@users.noreply.github.com>
2021-09-16 11:14:15 +0100

    Add file via upload
    Add all build scripts and test scripts

---
+++ buildConnect.sh
@@ -0,0 +1,10 @@
+APP_DIR=$HOME/idrs_poc
+SRC_DIR=$APP_DIR/src
+BIN_DIR=$APP_DIR/bin
+
+PGM="myODBCConnect"
+PGMSRC=$PGM".c"
+
+gcc -o $BIN_DIR/$PGM $SRC_DIR/$PGMSRC -lodbc
+
+echo "Program "$PGM" is built."
---
+++ testFetchRecord.sh
@@ -0,0 +1,9 @@
+APP_DIR=$HOME/idrs_poc
+SRC_DIR=$APP_DIR/src
+BIN_DIR=$APP_DIR/bin
+
+PGM="myODBCFetchRecord"
+
+$BIN_DIR/$PGM
+
+echo "Program "$PGM" is tested." 
